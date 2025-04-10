baeee876845e55d1425f10097915b8720881fdbc
Vladimira Rostekova <vladimira.rostekova@gmail.com>
2025-04-10 10:21:01 +0200

    pattern

--- searching.py
+++ searching.py
@@ -19,7 +19,8 @@ def read_data(file_name, field):
 
     with open (file_name, "r") as file_obj:
         data = json.load(file_obj)
-        return data[field]
+
+    return data[field]
 
 def linear_search(sequential_data, number):
     positions = []
@@ -29,11 +30,24 @@ def linear_search(sequential_data, number):
         if cislo == number:
             positions.append(idx)
             count += 1
+            #najhorsi aj najlepsi pripad asymp zlozitosti je rovnaky O(n)
+            #kazdy forcyklus vnasi O(n) zlozitost
 
     slovnik = { "positions": positions,
                 "count" : count
     }
     return slovnik
+
+def pattern_search(sequence, pattern):
+    indices = set()
+    #posuvanie o prvok doprava --> cez for cyklus
+
+    for idx_left in range(len(sequence)-len(pattern)+1): #zlozitst zavisi na dlzke sequence O(n) - linearne
+        if sequence[idx_left:idx_left + len(pattern)] == pattern: # zlozitst zavisi na dlzke toho patternu - prekazdy prvok musim porovnat ci sa zhoduje O(m) - tiez linearne
+            indices.add(idx_left + len(pattern) // 2)
+     #celkova zlozitost je: n*m
+
+    return indices
 
 
 def main():
@@ -42,6 +56,10 @@ def main():
 
     slovnik_dict = linear_search(sequential_data, 0)
     print(slovnik_dict)
+
+    pattern_data = read_data("sequential.json", "dna_sequence")
+    pattern_index = pattern_search(pattern_data, "ATA")
+    print(pattern_index)
 
 
 if __name__ == '__main__':
