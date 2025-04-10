ed3eb85ad6b146e92d56cd4716089bdbab3cf88a
Vladimira Rostekova <vladimira.rostekova@gmail.com>
2025-04-10 08:50:59 +0200

    jsonsubor

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,17 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    if field not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}:
+        return None
+
+    with open (file_name, "r") as file_obj:
+        data = json.load(file_obj)
+        return data[field]
+
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 
 if __name__ == '__main__':
